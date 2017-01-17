8fe6ab36288401a364c2bf31e02ae31999019cdf
MacDoc77 <vwvincentwang3@gmail.com>
2017-01-17 13:00:58 -0500

    Update Egg.java

--- src/Egg.java
+++ src/Egg.java
@@ -1,4 +1,19 @@
+public class Egg
+{
+    private int nutrition;
+    private double hatchTime;
+    private Hitbox box;
+    private Point pos;
+    private int xlocation, ylocation, length, width;
 
-public class Egg {
-
+    /**
+     * Constructor for objects of class Egg
+     */
+    public Egg(int nutrition, double hatchTime, Point pos)
+    {
+        this.nutrition = nutrition;
+        this.hatchTime = hatchTime;
+        this.pos = pos;
+        box = new Hitbox(pos.x - 1, pos.y - 1, 3, 3);
+    }
 }
