881bdc73958457d8b9521b1b866fd8676181bc49
MacDoc77 <vwvincentwang3@gmail.com>
2017-01-17 13:47:40 -0500

    Update Herbivore.java

--- src/Herbivore.java
+++ src/Herbivore.java
@@ -1,22 +1,31 @@
 public class Herbivore extends Organism
 {
     Hitbox box;
-    public Herbivore(double eggTime, double angle, double evolutionRate, boolean living, Hitbox hitbox)
+    public Herbivore(int size, double angle, double speed, double eggTime, double evolutionRate, boolean living, Hitbox hitbox)
     {
-        super(eggTime, angle, evolutionRate, living, hitbox);
+        super(size, angle, speed, eggTime, evolutionRate, living, hitbox);
         box = new Hitbox(pos.x - 4, pos.y - 4, 8, 8);
     }
     
-    public void eat(Food food){
-        //eats food
+    public void eat (Food food, int i){
+        food.remove(i);
     }
     
-    public void egg(){
+    public Egg placeEgg(Point pos){
+        return new Egg (/*hatchtime variable*/, pos);
     }
     
     public void evolve(){
     }
     
-    public boolean hitBoxCheck (Rectangle rec2){
+    public boolean hitBoxCheck(Rectangle rec2){
+        if ((box.getBox).intersects(rec2))
+            return true;
+        else 
+            return false;
+    }
+    
+    public void die(int i){
+        drawArea.herbivores.remove(i);
     }
 }
